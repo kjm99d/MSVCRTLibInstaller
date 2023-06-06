ad79f8d38f918e8f3b3106955a20f7c6390d8779
KJM <kjm99d@naver.com>
2023-06-06 23:41:53 +0900

    💄 설치 버전 선택 GUI 개발

--- resource.h
+++ resource.h
@@ -9,14 +9,20 @@
 #define IDR_MAINFRAME                   128
 #define IDC_PROGRESS1                   1000
 #define IDC_BUTTON1                     1001
+#define IDC_CHECK1                      1002
+#define IDC_CHECK2                      1003
+#define IDC_CHECK3                      1004
+#define IDC_CHECK4                      1005
+#define IDC_CHECK5                      1006
+#define IDC_CHECK6                      1007
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
